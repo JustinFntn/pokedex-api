5bff421822691f6cbc086d684dbfa4ba6c31715b
JustinFntn <jufo6216@gmail.com>
2024-04-17 17:26:28 +0200

    Ajouter la validation des pré-évolutions et post-évolutions dans la création de Pokémon

--- src/pokemons/pokemons.controller.js
+++ src/pokemons/pokemons.controller.js
@@ -6,6 +6,8 @@ const {
     deletePokemonModel,
 } = require(`./pokemons.model`);
 
+const { getTypeModel } = require(`../types/types.model`);
+
 const listPokemons = async (_req, res) => {
     try {
         const pokemons = await getPokemonsModel();
@@ -32,8 +34,47 @@ const getPokemonById = async (req, res) => {
 
 const createPokemon = async (req, res) => {
     try {
+        const { pre_evolution, post_evolution } = req.body;
+        if (pre_evolution) {
+            const prePokemon = await getPokemonModel(pre_evolution);
+            if (prePokemon.length === 0) {
+                return res
+                    .status(400)
+                    .send(
+                        `Pokemon with id ${pre_evolution} does not exist for pre_evolution`
+                    );
+            }
+        }
+        if (post_evolution) {
+            const postPokemon = await getPokemonModel(post_evolution);
+            if (postPokemon.length === 0) {
+                return res
+                    .status(400)
+                    .send(
+                        `Pokemon with id ${post_evolution} does not exist for post_evolution`
+                    );
+            }
+        }
+
+        const { type1_id, type2_id } = req.body;
+        if (type1_id) {
+            const type = await getTypeModel(type1_id);
+            if (type.length === 0) {
+                return res
+                    .status(400)
+                    .send(`Type with id ${type1_id} does not exist for type_1`);
+            }
+        }
+        if (type2_id) {
+            const type = await getTypeModel(type2_id);
+            if (type.length === 0) {
+                return res
+                    .status(400)
+                    .send(`Type with id ${type2_id} does not exist for type_2`);
+            }
+        }
         await addPokemonModel(req.body);
-        return res.status(200).end(req);
+        return res.status(200).end();
     } catch (err) {
         return res.status(500).end();
     }
