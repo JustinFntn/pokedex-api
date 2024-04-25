285a1b5435b416a9b69e5730f82186dfe9b2936d
JustinFntn <jufo6216@gmail.com>
2024-04-25 17:58:32 +0200

    Mise à jour de la documentation Swagger (ajout de components)

--- src/types/types.controller.js
+++ src/types/types.controller.js
@@ -1,20 +1,10 @@
 const { getTypesModel, getTypeModel } = require(`./types.model`);
 
-// const getTypesController = (_req, res) => {
-//     getTypesModel()
-//         .then((types) => {
-//             res.status(200).send(types);
-//         })
-//         .catch((err) => {
-//             res.status(500).end(err);
-//         });
-// };
-
 const getTypesController = async (_req, res) => {
     try {
         const types = await getTypesModel();
         if (types.length === 0) {
-            return res.status(204).send(types);
+            return res.status(204).send("No types found");
         }
         return res.status(200).send(types);
     } catch (err) {
